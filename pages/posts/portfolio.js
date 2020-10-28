b0625440ce4aebd5c2f35859cceb7c7506c2825c
Zach Perkins <partofaplan@gmail.com>
2020-10-28 16:05:38 -0500

    Added unordered list

--- pages/posts/portfolio.js
+++ pages/posts/portfolio.js
@@ -42,7 +42,18 @@ export default function FirstPost() {
          <h3>Profile</h3>
          <p>Experienced and reliable IT professional with over six years of experience working in a wide-range of enterprises to ensure the optimal performance of their application systems using automation and cloud technologies. Adept in providing solutions to improve and enhance services within a devops framework. Bringing forth a growth mindset and seeking to find the best path forward for the users.</p>
          <br />
-         
+         <h3>Experience</h3>
+         <h4>Infrastructure Automation Engineer at Boys Town</h4>
+              <p><ul><li>Created reliable automated workflows for service delivery of VMs and applications using vRealize Automation, Rundeck, and Powershell.</li>
+              <li>Helped develop Azure cloud app deployment pipeline for integration with the CI/CD pipeline web applications.</li>
+              <li>Improved Splunk infrastructure.</li>
+              <li>Improved VMware automated infrastructure.</li>
+              <li>Spearheaded integration of vRealize into ServiceNow.</li>
+              <li>Improved containerization infrastructure, formerly under OpenShift and shifting to PKS.</li>
+              <li>Created automation scripts in Javascript, Python, Bash and Powershell.</li>
+              <li>Lead an architecture guild made up of stakeholders, developers, security and operations to create a path for cohesive IT solutions which impact the business.</li>
+              </ul></p>
+          
        </motion.div>
        <h2>
          <Link href="/">
