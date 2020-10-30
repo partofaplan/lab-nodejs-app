1e2c331f5e032b6f24dfa4cfcbbfc05a3d7d0568
Zach Perkins <partofaplan@gmail.com>
2020-10-30 15:50:11 -0500

    Modulated test components of UI code

--- pages/posts/test.js
+++ pages/posts/test.js
@@ -1,8 +1,43 @@
 import * as React from "react"
 import { useState } from "react"
 import { motion, AnimatePresence } from "framer-motion"
+import { Button } from "@material-ui/core"
+import Head from "next/head"
+import Link from "next/link"
+import Layout from "../../components/layout"
 
-const Collapse = ({ isOpen }) => {
+export default function test() {
+  return (
+    <Layout>
+      <Head>
+        <title>Portfolio</title>
+      </Head>
+      <motion.div initial="hidden" animate="visible" variants={{
+        hidden: {
+          scale: .8,
+          opacity: 0
+        },
+        visible: {
+          scale: 1,
+          opacity: 1,
+          transition: {
+            delay: .5
+          }
+        },
+      }}>
+        <h2>Cloud & DevOps Engineer</h2>
+      </motion.div>
+      <Experience>
+        <CollapseExp />
+      </Experience>
+      <Education>
+        <CollapseEd />
+      </Education>
+    </Layout>
+  )
+};
+
+const CollapseExp = ({ isOpen }) => {
   return (
     <AnimatePresence initial={false}>
       {isOpen && (
@@ -16,36 +51,90 @@ const Collapse = ({ isOpen }) => {
             closed: { height: 0 }
           }}
           transition={{ duration: 0.25, clamp: true }}
-          >
-          <Contents />
+        >
+          <ContentsExp />
         </motion.section>
       )}
     </AnimatePresence>
   );
 };
 
-const Contents = () => (
-  <>
-    <p><ul><li>Created reliable automated workflows for service delivery of VMs and applications using vRealize Automation, Rundeck, and Powershell.</li>
-      <li>Helped develop Azure cloud app deployment pipeline for integration with the CI/CD pipeline web applications.</li>
-      <li>Improved Splunk infrastructure.</li>
-      <li>Improved VMware automated infrastructure.</li>
-      <li>Spearheaded integration of vRealize into ServiceNow.</li>
-      <li>Improved containerization infrastructure, formerly under OpenShift and shifting to PKS.</li>
-      <li>Created automation scripts in Javascript, Python, Bash and Powershell.</li>
-      <li>Lead an architecture guild made up of stakeholders, developers, security and operations to create a path for cohesive IT solutions which impact the business.</li>
-    </ul></p>
-  </>
-);
+const CollapseEd = ({ isOpen }) => {
+  return (
+    <AnimatePresence initial={false}>
+      {isOpen && (
+        <motion.section
+          key="content"
+          initial="closed"
+          animate="open"
+          exit="closed"
+          variants={{
+            open: { height: "auto" },
+            closed: { height: 0 }
+          }}
+          transition={{ duration: 0.25, clamp: true }}
+        >
+          <ContentsEd />
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
 
-export default function TestPage() {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleClick = () => setIsOpen(!isOpen);
-
+const ContentsExp = () => {
   return (
     <>
-      <button onClick={handleClick}>Test</button>
+      <h4>Infrastructure Automation Engineer at Boys Town</h4>
+      <p><ul><li>Created reliable automated workflows for service delivery of VMs and applications using vRealize Automation, Rundeck, and Powershell.</li>
+        <li>Helped develop Azure cloud app deployment pipeline for integration with the CI/CD pipeline web applications.</li>
+        <li>Improved Splunk infrastructure.</li>
+        <li>Improved VMware automated infrastructure.</li>
+        <li>Spearheaded integration of vRealize into ServiceNow.</li>
+        <li>Improved containerization infrastructure, formerly under OpenShift and shifting to PKS.</li>
+        <li>Created automation scripts in Javascript, Python, Bash and Powershell.</li>
+        <li>Lead an architecture guild made up of stakeholders, developers, security and operations to create a path for cohesive IT solutions which impact the business.</li>
+      </ul></p>
+    </>
+  );
+};
+
+const ContentsEd = () => {
+  return (
+    <>
+      <h4>Metropolitan Community College</h4>
+
+    </>
+  );
+};
+
+const Experience = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+  return (
+    <>
+      <Button variant="outlined" color="default" onClick={handleClick}><h3>Experience</h3></Button>
+      <CollapseExp isOpen={isOpen} />
+    </>
+  );
+};
+
+const Education = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+  return (
+    <>
+      <Button variant="outlined" color="default" onClick={handleClick}><h3>Education</h3></Button>
+      <CollapseEd isOpen={isOpen} />
+    </>
+  );
+};
+
+const Certifications = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+  return (
+    <>
+      <Button variant="outlined" color="default" onClick={handleClick}><h3>Certifications</h3></Button>
       <Collapse isOpen={isOpen} />
     </>
   );
